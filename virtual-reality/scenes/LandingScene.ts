f262d15f6815c329accfe7c01885327de59924e7
DavidHdzHdz <davidhhw24@gmail.com>
2022-08-25 20:24:20 -0500

    fit code to atomic design pattern

--- virtual-reality/scenes/LandingScene.ts
+++ virtual-reality/scenes/LandingScene.ts
@@ -1,40 +1,80 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+type Coordinate = [ number, number, number ]
 
 export class LandingScene {
-  private divElementHost?: HTMLDivElement;
+  private scene = new THREE.Scene();
+  private camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  private renderer = new THREE.WebGLRenderer()
+  private divHost?: HTMLDivElement;
 
   setDivElementHost(divElement: HTMLDivElement) {
-    this.divElementHost = divElement;
+    this.divHost = divElement;
   }
 
   renderScene() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.position.z = 3;
+    new OrbitControls(this.camera, this.renderer.domElement);
 
-    renderer.setSize( window.innerWidth, 300 );
+    const light = this.createWhiteLight([ -1, 2, 4 ]);
+    const cubes = [
+      this.createWireframeCube(0x00ff00, false, [ 0, 0, 0 ]),
+      this.createWireframeCube(0x8844aa, true, [ -2.5, 1, -1 ]),
+      this.createWireframeCube(0xaa8844, true, [ 2.5, -1, -1])
+    ]
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-    const cube = new THREE.Mesh( geometry, material );
+    cubes.forEach(cube => this.scene.add(cube));
+    this.scene.add(light);
+    this.renderCubesAnimation(cubes);
+    this.mountSceneToHost();
+  }
 
-    scene.add( cube );
-    camera.position.z = 2;
+  private createWireframeCube(color: THREE.ColorRepresentation, isPhong: boolean, coordinate: Coordinate) {
+    const MaterialTypeClass = isPhong ? THREE.MeshPhongMaterial : THREE.MeshBasicMaterial
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new MaterialTypeClass({ color, wireframe: true });
+    const cube = new THREE.Mesh(geometry, material);
 
-    const animate = () => {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.02;
-      cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
+    cube.position.set(...coordinate);
 
-    animate();
+    return cube;
+  }
 
-    // mount landing scene in de divHostElement provided
-    if (this.divElementHost) {
-      this.divElementHost.appendChild( renderer.domElement );
+  private createWhiteLight(coordinate: Coordinate) {
+    const color = 0xFFFFFF;
+    const intensity = 3;
+    const light = new THREE.DirectionalLight(color, intensity);
+
+    light.position.set(...coordinate);
+
+    return light;
+  }
+
+  private renderCubesAnimation(cubes: THREE.Mesh[]) {
+    const render = (time: number) => {
+      time *= 0.001;  // convert time to seconds
+
+      cubes.forEach((cube, ndx) => {
+        const speed = 1 + ndx * .1;
+        const rot = time * speed;
+        cube.rotation.x = rot;
+        cube.rotation.y = rot;
+      });
+
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+  }
+
+  private mountSceneToHost() {
+    if (this.divHost) {
+      this.divHost.appendChild(this.renderer.domElement);
     } else {
-      console.warn('[warning] Landing VR scene could not been rendered because divElementHost is not set yet');
+      console.warn('[waning] div host for VR experience was not provided before rendering');
     }
   }
 }
