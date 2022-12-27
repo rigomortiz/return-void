56269c124821d1a6bff208c38926a7aa59ebc074
Rigoberto Canseco <rigoberto.canseco.contractor@bbva.com>
2022-12-26 23:17:58 -0600

    add effect return void

--- components/molecules/ReturnVoidExperience/index.tsx
+++ components/molecules/ReturnVoidExperience/index.tsx
@@ -1,25 +1,40 @@
-import {useEffect, useRef} from "react";
-import styles from "../TvStaticExperience/index.module.css";
+import React, {useEffect, useRef} from "react";
 import {ReturnVoidScene} from "../../../virtual-reality/scenes/ReturnVoidScene";
+import Sky from "../../../pages/book/Sky";
+import Audio from "../../../pages/book/Audio";
 
-const ReturnVoid = () => {
-    const returnVoidVRExperienceHost = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (returnVoidVRExperienceHost.current !== null) {
-            const returnVoidScene = new ReturnVoidScene();
+const ReturnVoidExperience = () => {
 
-            returnVoidScene.setDivElementHost(returnVoidVRExperienceHost.current);
-            returnVoidScene.render();
-        }
-    }, []);
+  const returnVoidVRExperienceHost = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (returnVoidVRExperienceHost.current !== null) {
+      const returnVoidScene = new ReturnVoidScene();
+      returnVoidScene.setDivElementHost(returnVoidVRExperienceHost.current);
+      returnVoidScene.render();
+    }
+  }, []);
 
-    return (
-        <div ref={returnVoidVRExperienceHost}>
-            <div className={styles.body}><b>RETURN VOID</b></div>
+  return (
+    <>
+      <section className="hero is-fullheight return-void" ref={returnVoidVRExperienceHost}>
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <p className="title is-uppercase has-text-weight-bold is-size-1-mobile">RETURN<br/>VOID</p>
+            <p className="subtitle is-lowercase has-text-weight-light is-size-3 is-size-7-mobile has-text-white">
+              bad request & rigomortiz
+            </p>
+          </div>
         </div>
-    )
-
+        <div className="hero-foot">
+          <div className="content has-text-centered">
+            <Audio />
+          </div>
+        </div>
+        <Sky></Sky>
+      </section>
+    </>
+  )
 }
 
-export default ReturnVoid;
+export default ReturnVoidExperience;
