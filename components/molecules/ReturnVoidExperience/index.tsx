77a79187274dceb62bcb53f7c65b34badb89a223
Rigoberto Canseco <rigoberto.canseco.contractor@bbva.com>
2023-01-01 17:13:03 -0600

    custom return void experience

--- components/molecules/ReturnVoidExperience/index.tsx
+++ components/molecules/ReturnVoidExperience/index.tsx
@@ -13,20 +13,24 @@ import styles from "./index.module.scss";
 import BarGraphVisualizer from "../../effects/audio/Visualizer/BarGraph";
 import {Audio} from "../../core/Audio";
 import {CyberpunkSynthWave} from "../../core/types/enums/Colors";
+import OscilloscopeVisualizer from "../../effects/audio/Visualizer/Oscilloscope";
 
 
 const ReturnVoidExperience = ({section}: IReturnVoidEx) => {
-  let barGraphVisualizer: BarGraphVisualizer | null;
+  // let barGraphVisualizer: BarGraphVisualizer | null;
+  let oscilloscopeVisualizer: OscilloscopeVisualizer | null;
+
   const returnVoidVRExperienceHost = useRef<HTMLDivElement>(null);
   const style = {
     backgroundColor: CyberpunkSynthWave.Black,
-    barColor: CyberpunkSynthWave.PinkBite
+    // lineColor: CyberpunkSynthWave.PinkBite
+    lineColor: '#FFDE59'
   }
 
   function onClick() {
     const url = '/audio/Malfunction.mp3'
     Audio.play(url, (audio: Audio) => {
-      barGraphVisualizer!.run(audio, style)
+      oscilloscopeVisualizer!.run(audio, style)
       console.log(audio);
       document.querySelector(".button")!.setAttribute('disabled', 'disabled')
     })
@@ -36,7 +40,7 @@ const ReturnVoidExperience = ({section}: IReturnVoidEx) => {
     document.addEventListener('keydown', () => {
       const url = '/audio/Malfunction.mp3'
       Audio.play(url, (audio: Audio) => {
-        barGraphVisualizer!.run(audio, style)
+        oscilloscopeVisualizer!.run(audio, style)
         console.log(audio);
         document.querySelector(".button")!.setAttribute('disabled', 'disabled')
       })
@@ -61,12 +65,12 @@ const ReturnVoidExperience = ({section}: IReturnVoidEx) => {
           {/*<div ref={returnVoidVRExperienceHost}></div>*/}
           <Paragraph text={section.bodyHero.title}
                      classes={`${styles['title']} is-uppercase has-text-weight-bold is-size-1-mobile`} />
-          <br />
-          <Button classes={'is-danger is-rounded'} text={section.bodyHero.button.text} onClick={onClick}/>
-          {/*<p className={"press-key is-family-monospace"}><b>PRESS SPACE BUTTON</b></p>*/}
+          {/*<BarGraphVisualizer ref={ o => (barGraphVisualizer = o)} classes={''}/>*/}
+          <OscilloscopeVisualizer ref={ o => (oscilloscopeVisualizer = o)} classes={styles['visualizer']}/>
         </BodyHero>
         <FootHero>
-          <BarGraphVisualizer ref={ o => (barGraphVisualizer = o)} classes={''}/>
+          {/*<p className={"press-key is-family-monospace"}><b>PRESS SPACE BUTTON</b></p>*/}
+          <Button classes={'is-danger is-rounded'} text={section.bodyHero.button.text} onClick={onClick}/>
         </FootHero>
         <SkyEffect/>
       </SectionHero>
