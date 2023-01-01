d8c9a8e9c7740004f53195acb1bb26dbf73bd836
Rigoberto Canseco <rigoberto.canseco.contractor@bbva.com>
2023-01-01 17:13:51 -0600

    add xr module

--- pages/book/index.tsx
+++ pages/book/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useEffect} from "react";
 import BodyHero from "../../components/atoms/Section/Hero/Body";
 import Paragraph from "../../components/atoms/Paragraph";
 import SectionHero from "../../components/atoms/Section/Hero";
 import Link from "../../components/atoms/Link";
 
 const BookPage = () =>  {
+
+  useEffect(() => {
+    const xrSystem: XRSystem | undefined = navigator.xr
+    if (xrSystem == undefined) {
+      console.log('XRSystem is undefined')
+      return;
+    }
+    xrSystem!.isSessionSupported('inline')
+      .then(r => {
+        console.log(r)
+      })
+    xrSystem!.requestSession('inline', {
+        optionalFeatures: [ "local" ]
+      })
+      .then((xrSession: XRSession) => {
+        console.log(xrSession)
+        xrSession.requestAnimationFrame(d  => {
+          console.log(d)
+        })
+      }).catch(e => {
+        console.log(e)
+      }).finally( () => {
+          console.log('end session')
+      })
+
+
+  }, []);
 
   return (
     <SectionHero classes={'is-black is-fullheight'}>
