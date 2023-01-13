1b7e1515acd70f0ce25d88bb799efbc78584688a
Rigoberto Canseco <rigoberto.canseco.contractor@bbva.com>
2023-01-12 21:51:31 -0600

    add book cover effects

--- components/molecules/ReturnVoidExperience/index.tsx
+++ components/molecules/ReturnVoidExperience/index.tsx
@@ -6,31 +6,31 @@ import Paragraph from "../../atoms/Paragraph";
 import HeadHero from "../../atoms/Section/Hero/Head";
 import Button from "../../atoms/Button";
 import BodyHero from "../../atoms/Section/Hero/Body";
-import FootHero from "../../atoms/Section/Hero/Foot";
-import SkyEffect from "../../effects/graphics/Sky";
 import {IReturnVoidEx} from "./types";
 import styles from "./index.module.scss";
 import BarGraphVisualizer from "../../effects/audio/Visualizer/BarGraph";
 import {Audio} from "../../core/Audio";
 import {CyberpunkSynthWave} from "../../core/types/enums/Colors";
-import OscilloscopeVisualizer from "../../effects/audio/Visualizer/Oscilloscope";
+//import OscilloscopeVisualizer from "../../effects/audio/Visualizer/Oscilloscope";
+import Sky from "../../effects/graphics/Sky";
 
 
 const ReturnVoidExperience = ({section}: IReturnVoidEx) => {
-  // let barGraphVisualizer: BarGraphVisualizer | null;
-  let oscilloscopeVisualizer: OscilloscopeVisualizer | null;
+  let barGraphVisualizer: BarGraphVisualizer | null;
+  //let oscilloscopeVisualizer: OscilloscopeVisualizer | null;
 
   const returnVoidVRExperienceHost = useRef<HTMLDivElement>(null);
   const style = {
-    backgroundColor: CyberpunkSynthWave.Black,
-    // lineColor: CyberpunkSynthWave.PinkBite
-    lineColor: '#FFDE59'
+    backgroundColor: CyberpunkSynthWave.MidnightDreams,
+    lineColor: CyberpunkSynthWave.PinkBite,
+    barColor: CyberpunkSynthWave.PinkBite
+    //lineColor: '#FFDE59'
   }
 
   function onClick() {
     const url = '/audio/Malfunction.mp3'
     Audio.play(url, (audio: Audio) => {
-      oscilloscopeVisualizer!.run(audio, style)
+      barGraphVisualizer!.run(audio, style)
       console.log(audio);
       document.querySelector(".button")!.setAttribute('disabled', 'disabled')
     })
@@ -40,7 +40,7 @@ const ReturnVoidExperience = ({section}: IReturnVoidEx) => {
     document.addEventListener('keydown', () => {
       const url = '/audio/Malfunction.mp3'
       Audio.play(url, (audio: Audio) => {
-        oscilloscopeVisualizer!.run(audio, style)
+        barGraphVisualizer!.run(audio, style)
         console.log(audio);
         document.querySelector(".button")!.setAttribute('disabled', 'disabled')
       })
@@ -54,25 +54,27 @@ const ReturnVoidExperience = ({section}: IReturnVoidEx) => {
   }, []);
 
   return (
-      <SectionHero classes={`is-fullheight is-black`} >
+      <SectionHero classes={`is-fullheight is-black`}>
+          <Sky classes={styles['sky']}/>
         <HeadHero>
+            <BarGraphVisualizer ref={ o => (barGraphVisualizer = o)} classes={styles['visualizer']}/>
           <NavLevel>
-            <Paragraph text={section.headHero.title}
+              <Paragraph text={section.headHero.title}
                        classes={`level-item has-text-centered ${styles['subtitle']} is-lowercase has-text-weight-light is-size-3 is-size-5-mobile has-text-white`} />
           </NavLevel>
         </HeadHero>
         <BodyHero>
           {/*<div ref={returnVoidVRExperienceHost}></div>*/}
-          <Paragraph text={section.bodyHero.title}
+            <Button classes={`${styles['buttonPlay']} is-danger is-rounded`} text={section.bodyHero.button.text} onClick={onClick}/>
+
+            <Paragraph text={section.bodyHero.title}
                      classes={`${styles['title']} is-uppercase has-text-weight-bold is-size-1-mobile`} />
-          {/*<BarGraphVisualizer ref={ o => (barGraphVisualizer = o)} classes={''}/>*/}
-          <OscilloscopeVisualizer ref={ o => (oscilloscopeVisualizer = o)} classes={styles['visualizer']}/>
+          {/*<OscilloscopeVisualizer ref={ o => (oscilloscopeVisualizer = o)} classes={styles['visualizer']}/>*/}
         </BodyHero>
-        <FootHero>
-          {/*<p className={"press-key is-family-monospace"}><b>PRESS SPACE BUTTON</b></p>*/}
-          <Button classes={'is-danger is-rounded'} text={section.bodyHero.button.text} onClick={onClick}/>
-        </FootHero>
-        <SkyEffect/>
+        {/*<FootHero>*/}
+        {/*  <p className={"press-key is-family-monospace"}><b>PRESS SPACE BUTTON</b></p>*/}
+        {/*  <Button classes={'is-danger is-rounded'} text={section.bodyHero.button.text} onClick={onClick}/>*/}
+        {/*</FootHero>*/}
       </SectionHero>
   )
 }
